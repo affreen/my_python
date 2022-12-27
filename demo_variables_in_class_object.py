88b81a8f3702f4c7b9d6a703570891d141ba9920
Affreen <affreen.manish.jha@gmail.com>
2022-12-27 15:54:59 -0500

    demo instance variable and class variable in OOPs in Python

--- demo_variables_in_class_object.py
+++ demo_variables_in_class_object.py
@@ -1,3 +1,22 @@
 '''
  Python script to demo static variables and instance variables.
 '''
+
+class Car:
+    wheels = 4 # Static variable
+    def __init__(self):
+        self.mileage = 10 # Instance variable
+        self.model = 'BMW' # Instance variable
+
+c1 = Car()
+c2 = Car()
+
+print(c1.model, c1.mileage, c1.wheels)
+print(c2.model, c2.mileage, c2.wheels)
+
+print('Changing the value of the static variable wheels to 5 !')
+
+Car.wheels = 5
+
+print(c1.model, c1.mileage, c1.wheels)
+print(c2.model, c2.mileage, c2.wheels)
